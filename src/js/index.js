c8fb25f10b11c5b5a21196fb18b7a56734757d90
Leandro Gabriel <leandro.gabriel69@hotmail.com>
2023-05-29 12:59:55 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+// aspas dupla: ""
+
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body")
+const imagemBotaoTrocarDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+    
+    body.classList.toggle("modo-escuro");
+
+        if(modoEscuroEstaAtivo){
+            imagemBotaoTrocarDeTema.setAttribute("src", "/src/imagens/moon.png");
+        } else{
+            imagemBotaoTrocarDeTema.setAttribute("src", "/src/imagens/sun.png");
+        }
+});
+
+
+
+
+
+
+
+
